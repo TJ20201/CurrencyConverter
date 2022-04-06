2ad834f4e361af7f64593d198a35af5f057cefbe
TJ20201 <86313252+TJ20201@users.noreply.github.com>
2022-04-06 16:56:20 +0100

    feat: Create repository with files from local storage

---
+++ README.md
@@ -0,0 +1,12 @@
+# Currency Converter
+
+This is a currency converter made with Python's Tkinter library.
+
+## Installation
+
+Add `currency_converter.py` and `currency_data.json` into the same folder as eachother.
+
+## Usage
+### Windows
+`py -3 currency_converter.py`
+
---
+++ currency_converter.py
@@ -0,0 +1,77 @@
+import tkinter as tk
+import json
+
+currencys = ['CZK', 'GBP', 'USD']
+            #'FROM_TO': VALUE
+
+datajson = json.load(open('currency_data.json'))
+rates = datajson['rates']
+
+colours = {
+ 'BLACK': '#28282B',
+ 'BLACM': '#18171B',
+ 'BLACD': '#17171A',
+ 'WHITE': '#FDFBF9',
+ 'GREEN': '#48A14D',
+ 'GREED': '#37903C',
+ 'RED':   '#AB7473'
+}
+
+width, height = 350, 200
+
+def stringToNumber(string: str) -> float:
+ try: return float(string.replace(',',''))
+ except ValueError:return 0.0
+
+root = tk.Tk()
+# Move Window
+def move_window(event):
+ x,y = event.x_root-10,event.y_root-10
+ root.geometry('+{0}+{1}'.format(x,y))
+root.overrideredirect(True)
+# Title Bar
+titleBarHeight = 25
+titleBar = tk.Frame(root, bg=colours['BLACM'])
+titleBarLabel = tk.Label(titleBar, bg=colours['BLACM'],fg=colours['WHITE'],text='Currency Converter')
+buttonClose = tk.Button(titleBar, width=int(titleBarHeight/8), height=titleBarHeight, text='X', bg=colours['RED'], activebackground='#9A6362', fg=colours['WHITE'], activeforeground=colours['BLACK'], command=root.destroy)
+titleBar.bind('<B1-Motion>', move_window);titleBarLabel.bind('<B1-Motion>', move_window);
+titleBar.place(height=titleBarHeight,width=width)
+titleBarLabel.place(height=titleBarHeight, width=105,x=titleBarHeight)
+buttonClose.pack(side=tk.RIGHT)
+
+root.geometry(f"{width}x{height+titleBarHeight}")
+root.geometry(f"+50+50")
+
+main = tk.Frame(bg=colours['BLACK'])
+main.place(height=height,width=width,y=titleBarHeight)
+
+intype = tk.StringVar();intype.set("GBP")
+outype = tk.StringVar();outype.set("USD")
+
+curin = tk.Entry(root,bg=colours['BLACD'],fg=colours['WHITE']) 
+curin.place(width=width-73,x=5,y=5+titleBarHeight)
+
+valin = tk.OptionMenu(root, intype, *currencys)
+valin.config(highlightthickness=0,bg=colours['BLACD'],fg=colours['WHITE'],activebackground=colours['BLACM'])
+valin["menu"].config(bg=colours['BLACD'],fg=colours['WHITE'],activebackground=colours['BLACM'])
+valin["menu"]["borderwidth"] = valin["borderwidth"] = 0
+valin.place(x=width-63,y=5+titleBarHeight, height=20)
+valou = tk.OptionMenu(root, outype, *currencys)
+valou.config(highlightthickness=0,bg=colours['BLACD'],fg=colours['WHITE'],activebackground=colours['BLACM'])
+valou["menu"].config(bg=colours['BLACD'],fg=colours['WHITE'],activebackground=colours['BLACM'])
+valou["menu"]["borderwidth"] = valou["borderwidth"] = 0
+valou.place(x=5,y=35+titleBarHeight, height=20,width=width-10)
+final = tk.Label(root, fg=colours['WHITE'], bg=colours['BLACD'])
+final.place(x=5, y=105+titleBarHeight, width=width-10,height=height-150)
+
+def convertMoney():
+ try:rate = rates[f'{intype.get()}_{outype.get()}']
+ except KeyError:rate = rates[f'{outype.get()}_{intype.get()}']
+ final.configure(text=f"""~{round(stringToNumber(curin.get()),2)} {intype.get()}
+  ~{round(stringToNumber(curin.get())*rate,2)} {outype.get()}
+ """)
+ 
+convert = tk.Button(root,fg=colours['WHITE'],activeforeground=colours['WHITE'],bg=colours['GREEN'],activebackground=colours['GREED'],text=f"Convert currency",command=convertMoney)
+convert.place(x=width/4+5,y=75+titleBarHeight,width=width/2-5)
+
+root.mainloop()
